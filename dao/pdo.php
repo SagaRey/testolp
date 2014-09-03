e7035f4b294f166bd61bf12baebc1d5ea020b8ff
saga.rey <saga.rey@outlook.com>
2014-09-03 16:38:26 +0800

    DAO,SQL SET

--- dao/pdo.php
+++ dao/pdo.php
@@ -6,10 +6,10 @@
 	// $password = 'R8YM8EHC36cl4xaGMdSirajFC7Fixz43';
 	// $host = 'sqld.duapp.com';
 	// $port = '4050';
-	$dbname = 'testol';
-	$user = 'adminJH2jQv5';
-	$password = 'DHfIE7NycCs9';
-	$host = '127.8.136.130';
+	$dbname = 'testolp';
+	$user = 'adminjfN2FHM';
+	$password = 'sMMmabQNQz7j';
+	$host = '127.7.112.130';
 	$port = '3306';
 	$dsn = "$dbms:host=$host;port=$port;dbname=$dbname"; //Data Source Name 若更改过默认数据库端口，需加上端口号
 	try {
