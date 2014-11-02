c1f63e4553796238a54d65bb21c7226fbe89fa54
SagaRey <saga.rey@outlook.com>
2014-11-02 23:39:54 +0800

    index.php title form "index" -> "TestOl"

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="zh-cn">
   <head>
-    <title>Index</title>
+    <title>TestOL</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="./css/bootstrap.css">
     <style type="text/css">
